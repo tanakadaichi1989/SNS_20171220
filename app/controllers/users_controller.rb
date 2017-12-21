5ee47b59e2413062b0649d4716d3ccb73586e27f
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2017-12-21 19:36:12 +0900

    authenticate user reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user
+  #before_action :authenticate_user
   before_action :forbid_login_user,only:[:new,:create,:login_form,:login]
   before_action :ensure_correct_user,only:[:edit,:update]
 
