85a7edfe8c05bd6d659d4a86c36df3a103ae6e1d
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2017-12-21 19:45:27 +0900

    authenticate user user_controller only:[:index,:show,:edit,:update,:destroy]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
-  before_action :authenticate_user
+  #before_action :authenticate_user
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  #before_action :authenticate_user
+  before_action :authenticate_user
 
   def index
     @posts = Post.all.order(created_at: :desc)
