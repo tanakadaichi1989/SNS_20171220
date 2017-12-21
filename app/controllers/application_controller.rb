da5478d49b58d3b99d9f3e15f88d30352648003c
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2017-12-21 19:29:04 +0900

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
-  #before_action :authenticate_user
+  before_action :authenticate_user
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
 
   def authenticate_user
-      if @current_user == nil
+      if @current_user != nil
         flash[:notice] = "You need LOGIN or SIGNUP."
         redirect_to("/login")
       end
