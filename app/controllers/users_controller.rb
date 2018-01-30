a59ec30e00e14d24290f12179f049b3dc2e7b7bc
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-30 19:03:58 +0900

    Edit users controller @user && @user.authenticate(params[:password])

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,7 +76,7 @@ class UsersController < ApplicationController
    def login
     @user = User.find_by(email: params[:email])
 
-    if @user
+if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[notice] = "login."
       redirect_to("/")
