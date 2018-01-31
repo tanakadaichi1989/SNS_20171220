e48baec2526db905bfcfba2018036430089907ce
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-31 17:21:36 +0900

    Page name provide / Edit User info form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def full_page(title="")
+        base_title = "SNS by Ruby on Rails"
+        
+        if title.empty?
+            base_title
+        else
+            title + " | " + base_title
+        end
+    end
 end
